2e04b9a80776b6f9488055dcfbc758170fc2f465
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-12-05 15:32:09 +0000

    sound added

--- js/scores.js
+++ js/scores.js
@@ -1,13 +1,14 @@
 const highscores = document.getElementById("highscores");
 const clearBtn = document.getElementById("clear");
+
 let keys = Object.keys(localStorage);
 let li;
 let length  = keys.length;
 
 keys.forEach(()=>{
 li = document.createElement("li");
-console.log(keys);
-li.innerText = localStorage.getItem(keys[length]);
+console.log(keys[1]);
+li.innerText = `${keys[length -1]} - ${localStorage.getItem(keys[length-1])}`;
 highscores.appendChild(li);
 length--;
 });

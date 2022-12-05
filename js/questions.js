1d9d289faeadb7d828102f997fc8a67a7da86e35
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-12-05 14:35:34 +0000

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,133 @@
+const data = {
+    "Question": [
+      {
+        "Q": "Javascript is an _______ language?",
+        "A":[ {
+          "option":"Object oriented",
+          "correct": "true"
+        },
+        {
+            "option": "Object based",
+            "correct": "false"
+        },
+        {
+            "option": "Procedural",
+            "correct": "false"
+        },
+        {
+            "option": "None of the above",
+            "correct": "false"
+        }           
+        ]
+      },
+      {
+        "Q": "Which of the following keywords is used to define a variable in Javascript?",
+        "A": [
+            {
+                "option": "var",
+                "correct": "false"
+            },
+            {
+                "option": "let",
+                "correct": "false"
+            },
+            {
+                "option": "Both A & B",
+                "correct": "true"
+            },
+            {
+                "option": "None of the above",
+                "correct": "false"
+            }
+          
+        ]
+      },
+      {
+        "Q": "Which of the following methods is used to access HTML elements using Javascript?",
+        "A": [
+            {
+                "option": "getElementbyId()",
+                "correct": "false"
+            },
+            {
+                "option": "getElementsByClassName()",
+                "correct": "false"
+            },
+            {
+                "option": "Both a & b",
+                "correct": "true"
+            },
+            {
+                "option": "None of the above",
+                "correct": "false"
+            }
+          
+        ]
+      },
+      {
+        "Q": "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        "A": [
+            {
+                "option": "Throws an error",
+                "correct": "false"
+            },
+            {
+                "option": "Ignores the statements",
+                "correct": "true"
+            },
+            {
+                "option": "Gives a warning",
+                "correct": "false"
+            },
+            {
+                "option": "None of the above",
+                "correct": "false"
+            }
+        ]
+      },
+      {
+        "Q": "Which of the following methods can be used to display data in some form using Javascript?",
+        "A": [
+            {
+                "option":  "dcument.write()",
+                "correct": "false"
+            },
+            {
+                "option":  "console.log()",
+                "correct": "false"
+            },
+            {
+                "option":  "window.alert()",
+                "correct": "false"
+            },
+            {
+                "option": "All of the above",
+                "correct": "true"
+            }    
+          
+        ]
+      },
+      {
+        "Q": "How can a datatype be declared to be a constant type?",
+        "A": [
+            {
+                "option": "var",
+                "correct": "false"
+            },
+            {
+                "option": "const",
+                "correct": "true"
+            },
+            {
+                "option": "let",
+                "correct": "false"
+            },
+            {
+                "option": "int",
+                "correct": "false"
+            }
+          
+        ]
+      }
+    ]
+  }
